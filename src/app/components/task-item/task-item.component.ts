dcbb3f5d32891efdcc40c01ec2346edc1ed12e4d
duterte <ochea.duterte@gmail.com>
2022-04-16 11:46:37 +0800

    finished tutorial

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css'],
+})
+export class TaskItemComponent implements OnInit {
+  @Input() task: Task = {
+    id: 0,
+    text: 'Initial Task',
+    day: 'April 17, 2022',
+    reminder: false,
+  };
+  faTimes = faTimes;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onDelete(task: Task) {
+    this.onDeleteTask.emit(task);
+  }
+  onToggle(task: Task) {
+    this.onToggleReminder.emit(task);
+  }
+}
